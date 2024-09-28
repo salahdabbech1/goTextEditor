2bd42f92686c9c5f51c9c06f5d5864024e696e73
salahdabbech1 <salah.dabbech@esprit.tn>
2024-09-28 18:35:22 +0100

    feat: We Are now Showing Hello World with asteriks !!

--- main.go
+++ main.go
@@ -4,16 +4,47 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/mattn/go-runewidth"
 	"github.com/nsf/termbox-go"
 )
 
-func print_message(column, row int, foregound, background termbox.Attribute, message string) {
-	for _, character := range message {
-		termbox.SetCell(column, row, character, foregound, background)
-		column += runewidth.RuneWidth(character)
-	}
+var ROWS, COLS int
+var offsetX, offsetY int
+
+var text_buffer = [][]rune{
+	{'h', 'e', 'l', 'l', 'o'},
+	{'w', 'o', 'r', 'l', 'd'},
 }
+
+func display_text_buffer() {
+	var row, col int
+	for row = 0; row < ROWS; row++ {
+		text_buffer_row := row + offsetY
+		for col = 0; col < COLS; col++ {
+			text_buffer_col := col + offsetX
+			if text_buffer_row >= 0 && text_buffer_row < len(text_buffer) && text_buffer_col < len(text_buffer[text_buffer_row]) {
+				if (text_buffer[text_buffer_row][text_buffer_col]) != '\t' {
+					termbox.SetChar(col, row, text_buffer[text_buffer_row][text_buffer_col])
+				} else {
+					termbox.SetCell(col, row, rune(' '), termbox.ColorDefault, termbox.ColorGreen)
+				}
+			} else if row+offsetY > len(text_buffer)-1 {
+				termbox.SetCell(0, row, rune('*'), termbox.ColorCyan, termbox.ColorDefault)
+				//termbox.SetChar(col, row, rune('\n'))
+			}
+
+		}
+	}
+
+}
+
+/*
+	func print_message(col, row int, foregound, background termbox.Attribute, message string) {
+		for _, character := range message {
+			termbox.SetCell(col, row, character, foregound, background)
+			col += runewidth.RuneWidth(character)
+		}
+	}
+*/
 func run_editor() {
 	err := termbox.Init()
 	if err != nil {
@@ -21,7 +52,13 @@ func run_editor() {
 		os.Exit(1)
 	}
 	for {
-		print_message(0, 0, termbox.ColorDefault, termbox.ColorDefault, "First time trying a text editor")
+		ROWS, COLS = termbox.Size()
+		ROWS--
+		if COLS < 80 {
+			COLS = 80
+		}
+		termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+		display_text_buffer()
 		termbox.Flush()
 		event := termbox.PollEvent()
 		if event.Type == termbox.EventKey && event.Key == termbox.KeyEsc {
